5e1e1c66655b12c0196ae5a6040f7ed8880ab751
lmbugua <lee.mbugua@gmail.com>
2021-11-29 18:49:56 -0800

    spruce

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ function HomePage() {
 
       <div className="container max-w-4xl m-auto px-4 mt-20">
         <Image
-          src="/desk.jpg"
+          src="/beach.jpg"
           alt="my desk"
           width={1920 / 2}
           height={1280 / 2}
